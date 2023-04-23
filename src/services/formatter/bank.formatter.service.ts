9c0a8901ebeef681f3e880bf3429eb9546f9d9fa
cipher-tech <nickchibuikem@gmail.com>
2023-04-23 23:24:32 +0100

    added customer formatter

--- src/services/formatter/bank.formatter.service.ts
+++ src/services/formatter/bank.formatter.service.ts
@@ -1,4 +1,4 @@
-import { IAccount } from "../../interfaces/user.interface";
+import { IAccount, ICustomer, ICustomerScrapTemplate } from "../../interfaces/user.interface";
 import HashText from "../../util/hashText.helper";
 import { replaceString } from "../../util/replace.string.util";
 
@@ -15,14 +15,37 @@ interface accountFormatter {
     accountType: string;
     bank: string;
 }
-export class BankFormatter {
+export class Formatter {
     async authFormatter(options: AuthFormatter) {
-        const {token, passCode, platform} = options
+        const { token, passCode, platform } = options
         return {
-            token, 
+            token,
             passCode: await HashText.getHash(passCode),
-            otp: 12345, 
+            otp: 12345,
             platform
+        }
+    }
+    customerFormatter(options: Partial<ICustomerScrapTemplate>): ICustomer {
+        const {
+            firstName,
+            lastName,
+            name,
+            bvn = "",
+            phoneNumber = "",
+            address,
+            apartmentNumber,
+            street,
+            state,
+            country,
+            email = "" } = options
+        return {
+            //if name is already combined continue else merge first and last name
+            name: name ? name.trim() : `${ firstName } ${ lastName }`.trim(),
+            //if address is combined continue else merge apartmentNumber, street, state and country
+            address: address ? address.trim() : `${ apartmentNumber } ${ street } ${ state } ${ country }`.trim(),
+            bvn: bvn.trim(),
+            phoneNumber: phoneNumber.trim(),
+            email: email.trim()
         }
     }
     accountFormatter(account: accountFormatter): IAccount {
