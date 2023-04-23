1cec4714a1e3eb4d485b360f2a7980f4e0a0c942
cipher-tech <nickchibuikem@gmail.com>
2023-04-23 22:08:20 +0100

    updated interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -6,16 +6,27 @@ export interface ICustomer {
     email: string
 }
 
+export interface IAuth {
+    token: string,
+    passCode: string,
+    platform: string
+    otp: number
+}
+
 export interface IAccount {
     title: string
     amount: number
-    balance: number
-    transactions: ITransaction[]
+    ledgerBalance: number
+    currency: string
+    symbol: string
+    bank: string
+    accountNumber?: string
+    accountType: string
 }
 
 export interface ITransaction {
     type: string
-    date: string
+    approvalDate: string
     description: string
     amount: number
     beneficiary: string
