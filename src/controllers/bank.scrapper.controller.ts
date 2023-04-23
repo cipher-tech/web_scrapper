34031e14d1ded18318bf9f06e5b848edc2e43237
cipher-tech <nickchibuikem@gmail.com>
2023-04-23 22:36:39 +0100

    refactored code

--- src/controllers/bank.scrapper.controller.ts
+++ src/controllers/bank.scrapper.controller.ts
@@ -2,14 +2,14 @@ import { ISupportedBanks } from "../interfaces/supported.banks.interface"
 import { supportedBanks } from "../services";
 import { CustomerAuthService } from "../services/bank-scrappers/auth.custormer.service";
 
-type scrapBankOptions = {
+export interface IScrapBankOptions {
     bank: keyof ISupportedBanks,
     url: string,
     token: string,
     passCode: string
 }
 interface IBankScrapper {
-    scrapBank: (options: scrapBankOptions) => void;
+    scrapBank: (options: IScrapBankOptions) => void;
 }
 
 /**
@@ -20,13 +20,14 @@ interface IBankScrapper {
  */
 
 export class BankScrapperController implements IBankScrapper {
-    async scrapBank(options: scrapBankOptions) {
+    async scrapBank(options: IScrapBankOptions) {
         const customerAuth = new CustomerAuthService()
         const auth = await customerAuth.authenticateCustomer(options.token, options.passCode, options.bank)
 
+        // get scrapper service for a particular bank
         const scrapperClass = supportedBanks[ options.bank ];
         const scrapper = await new scrapperClass();
 
-        scrapper.run({ url: options.url, bank: options.bank })
+        scrapper.run({ auth, ...options })
     }
 }
