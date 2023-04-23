bf414b95f3c354aac7b0ea0e858cdb83b68b0013
cipher-tech <nickchibuikem@gmail.com>
2023-04-23 11:25:53 +0100

    updated index file

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 import dotenv from "dotenv";
-import loggerInit from "./config/logger";
+// import loggerInit from "./config/logger";
 // initialize dotenv before importing config to ensure it gets populated.
 dotenv.config()
 
 // initiated dotenv before importing config to ensure it gets populated.
 // eslint-disable-next-line import/first
-import config from "./config";
+// import config from "./config";
 import { BankScrapperController } from "./controllers";
 
-let logger;
-// initialize logger for the right environment
-if (config?.ENVIRONMENT === "development") {
-    logger = loggerInit("development");
-} else if (config?.ENVIRONMENT === "production") {
-    logger = loggerInit("production");
-} else {
-    logger = loggerInit("");
-}
+// var logger;
+// // initialize logger for the right environment
+// if (config?.ENVIRONMENT === "development") {
+//     logger = loggerInit("development");
+// } else if (config?.ENVIRONMENT === "production") {
+//     logger = loggerInit("production");
+// } else {
+//     logger = loggerInit("");
+// }
 
-logger.info("Project initialized")
+// global.logger = logger;
+// logger.info("Project initialized")
 
 const bankScrapper = new BankScrapperController()
-bankScrapper.scrapBank({bank: "bank_of_okra"})
+bankScrapper.scrapBank({bank: "bank_of_okra", url: 'https://bankof.okra.ng'})
