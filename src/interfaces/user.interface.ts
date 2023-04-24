187c63df68a3a109308b7ce34f3935862ad29917
cipher-tech <nickchibuikem@gmail.com>
2023-04-24 08:26:24 +0100

    added template interfaces

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,27 +1,15 @@
 // Standard Customer format after formatting
 export interface ICustomer {
+    _id?: string;
+    auth?: IAuth['_id']
     name: string
     address: string
     bvn: string
     phoneNumber: string
     email: string
 }
-
-// general template to populate after scrapping for customer information
-export interface ICustomerScrapTemplate {
-    firstName: string,
-    lastName: string,
-    name: string,
-    bvn: string,
-    phoneNumber: string,
-    email: string,
-    address: string,
-    apartmentNumber: string,
-    street: string
-    state: string,
-    country: string
-}
 export interface IAuth {
+    _id?: string
     token: string,
     passCode: string,
     platform: string
@@ -29,8 +17,9 @@ export interface IAuth {
 }
 
 export interface IAccount {
+    customer: ICustomer["_id"]
     title: string
-    amount: number
+    accountBalance: number
     ledgerBalance: number
     currency: string
     symbol: string
@@ -46,4 +35,29 @@ export interface ITransaction {
     amount: number
     beneficiary: string
     sender: string
+}
+// general template to populate after scrapping for customer information
+export interface ICustomerScrapTemplate {
+    firstName: string,
+    lastName: string,
+    name: string,
+    bvn: string,
+    phoneNumber: string,
+    email: string,
+    address: string,
+    apartmentNumber: string,
+    street: string
+    state: string,
+    country: string
+}
+export interface IAccountScrapTemplate {
+    accountName: string
+    title: string
+    accountBalance: string
+    ledgerBalance: string
+    currency: string
+    symbol: string
+    bank: string
+    accountNumber?: string
+    accountType: string
 }
