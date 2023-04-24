0cfb07c4582860adf5de7af4b8f84358ec497619
cipher-tech <nickchibuikem@gmail.com>
2023-04-24 23:41:17 +0100

    refactored controller

--- src/controllers/bank.scrapper.controller.ts
+++ src/controllers/bank.scrapper.controller.ts
@@ -1,15 +1,6 @@
-import { Request, Response } from 'express';
-import config from "../config";
-import { ISupportedBanks } from "../interfaces/supported.banks.interface"
+import { Response } from 'express';
 import { supportedBanks } from "../services";
-import { CustomerAuthService } from "../services/bank-scrappers/auth.custormer.service";
-
-export interface IScrapBankOptions {
-    bank: keyof ISupportedBanks,
-    url: string,
-    token: string,
-    passCode: string
-}
+import { IRequestWithAuth } from '../middleware/auth.middleware';
 
 /**
  * Bank scrapper class
@@ -17,26 +8,18 @@ export interface IScrapBankOptions {
  *
  * @returns {object} BankScrapperController
  */
-
 export class BankScrapperController{
-    async scrapBank(req: Request, res: Response) {
+    async scrapBank(req: IRequestWithAuth, res: Response) {
         try {
-            console.log("::::::::: req scrap_test_bank", req.body);
-            const options: IScrapBankOptions = { 
-                bank: "bank_of_okra",
-                url: config?.TEST_URL || 'https://bankof.okra.ng',
-                token: req?.body?.token || config?.EMAIL,
-                passCode: req?.body?.passCode || config?.PASSWORD,
-            } 
-            
-            const customerAuth = new CustomerAuthService()
-            const auth = await customerAuth.authenticateCustomer(options.token, options.passCode, options.bank)
-    
+            if (!req?.options?.bank || !req.auth) {
+            throw new Error("Something went wrong, please try again.")
+            }
             // get scrapper service for a particular bank
-            const scrapperClass = supportedBanks[ options.bank ];
+            const scrapperClass = supportedBanks[ req.options.bank ];
             const scrapper = await new scrapperClass();
     
-            const result = await scrapper.run({ auth, ...options })
+            // scrap the test website
+            const result = await scrapper.run({ auth: req.auth, ...req.options })
     
             return res.json(result) 
         } catch (error) {
