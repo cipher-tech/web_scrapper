aeebb2f1932d78e2c78700f48ee933e850e4a57d
cipher-tech <nickchibuikem@gmail.com>
2023-04-24 08:28:37 +0100

    added helper to replace special characters

--- src/util/replace.string.util.ts
+++ src/util/replace.string.util.ts
@@ -1,3 +1,7 @@
 export const replaceString = (string: string, target = '$', value = '') => {
-    return (`${string}` || '').replace(new RegExp(`[${target}]`, 'g'), value).trim();
+    return (`${string}` || '').replace(new RegExp(`${target}`, 'g'), value).trim();
+}
+
+export const replaceSpecialCharacters = (string: string, ) => {
+    return (string).replace(/[^a-zA-Z0-9 ]/g, '');
 }
