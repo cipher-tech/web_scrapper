637a3eaf62055f3123c45d578fdb7f8bd3cee59d
cipher-tech <nickchibuikem@gmail.com>
2023-04-24 17:10:30 +0100

    refactored code

--- src/controllers/bank.scrapper.controller.ts
+++ src/controllers/bank.scrapper.controller.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from 'express';
+import config from "../config";
 import { ISupportedBanks } from "../interfaces/supported.banks.interface"
 import { supportedBanks } from "../services";
 import { CustomerAuthService } from "../services/bank-scrappers/auth.custormer.service";
@@ -8,9 +10,6 @@ export interface IScrapBankOptions {
     token: string,
     passCode: string
 }
-interface IBankScrapper {
-    scrapBank: (options: IScrapBankOptions) => void;
-}
 
 /**
  * Bank scrapper class
@@ -19,15 +18,30 @@ interface IBankScrapper {
  * @returns {object} BankScrapperController
  */
 
-export class BankScrapperController implements IBankScrapper {
-    async scrapBank(options: IScrapBankOptions) {
-        const customerAuth = new CustomerAuthService()
-        const auth = await customerAuth.authenticateCustomer(options.token, options.passCode, options.bank)
-
-        // get scrapper service for a particular bank
-        const scrapperClass = supportedBanks[ options.bank ];
-        const scrapper = await new scrapperClass();
-
-        scrapper.run({ auth, ...options })
+export class BankScrapperController{
+    async scrapBank(req: Request, res: Response) {
+        try {
+            console.log("::::::::: req scrap_test_bank", req.body);
+            const options: IScrapBankOptions = { 
+                bank: "bank_of_okra",
+                url: config?.TEST_URL || 'https://bankof.okra.ng',
+                token: req?.body?.token || config?.EMAIL,
+                passCode: req.body.passCode || config?.PASSWORD,
+            } 
+            
+            const customerAuth = new CustomerAuthService()
+            const auth = await customerAuth.authenticateCustomer(options.token, options.passCode, options.bank)
+    
+            // get scrapper service for a particular bank
+            const scrapperClass = supportedBanks[ options.bank ];
+            const scrapper = await new scrapperClass();
+    
+            const result = await scrapper.run({ auth, ...options })
+    
+            return res.json(result) 
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json("Could not scrap test bank, please try again.")
+        }
     }
 }
