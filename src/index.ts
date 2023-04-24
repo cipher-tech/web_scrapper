10366d015879c7ea3715428faffb095897dc1804
cipher-tech <nickchibuikem@gmail.com>
2023-04-24 17:09:07 +0100

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 // import loggerInit from "./config/logger";
@@ -9,6 +10,7 @@ dotenv.config()
 // import config from "./config";
 import { BankScrapperController } from "./controllers";
 import config from "./config";
+import { Server } from "http";
 
 // var logger;
 // // initialize logger for the right environment
@@ -23,19 +25,50 @@ import config from "./config";
 // global.logger = logger;
 // logger.info("Project initialized")
 
-main().catch(err => console.log(err));
+const app = express();
+let server: Server;
+
+app.use(express.json());
+app.post('/api/scrap_test_bank', (req, res) => {
+    const bankScrapper = new BankScrapperController()
+    bankScrapper.scrapBank(req, res)
+})
 
 async function main() {
-    if(!config?.MONGODB_URL){
+    if (!config?.MONGODB_URL) {
         throw new Error("Could not load config file")
     }
     await mongoose.connect(config.MONGODB_URL);
     console.log("database connected...");
-    
-    const bankScrapper = new BankScrapperController()
-    bankScrapper.scrapBank({ bank: "bank_of_okra", 
-    url: 'https://bankof.okra.ng',
-    token: config.EMAIL || '',
-    passCode: config.PASSWORD || '',
-})
 }
+
+main()
+    .then(() => {
+        const port = config?.PORT || 4000;
+        server = app.listen(port, () => {
+            console.log(`Server is listening on port ${ port }`);
+        })
+    })
+    .catch(err => console.log(err));
+
+
+
+    const exitHandler = () => {
+        if (server) {
+          server.close(() => {
+            console.log('Server closed');
+            process.exit(1);
+          });
+        } else {
+          process.exit(1);
+        }
+      };
+      
+      const unexpectedErrorHandler = (error: Error) => {
+        console.log(error);
+        exitHandler();
+      };
+      
+      process.on('uncaughtException', unexpectedErrorHandler);
+      process.on('unhandledRejection', unexpectedErrorHandler);
+      
